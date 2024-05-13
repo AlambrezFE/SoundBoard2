ed14614ed82abd3d22c4b6b78aa2585f3c084aea
AlambrezFE <andre29flores.aga.nst@gmail.com>
2024-05-13 15:27:11 -0500

    no message

--- FloresEnaoSoundBar1/ViewController.swift
+++ FloresEnaoSoundBar1/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tablaGrabaciones: UITableView!
+    
+    var grabaciones:[Grabacion] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            grabaciones = try context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        }catch{}
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let grabacion = grabaciones[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(grabacion)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                grabaciones = try context.fetch(Grabacion.fetchRequest())
+                tablaGrabaciones.reloadData()
+            }catch()
+        }
+    }
 
 }
 
